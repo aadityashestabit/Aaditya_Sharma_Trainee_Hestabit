8bd84008aaf7329cc67c11b770ac77de77f307d1
Aaditya Sharma <aadityas.hestabit@gmail.com>
2026-02-05 13:33:07 +0530

    Correct error in app.js

--- Week_1/Day3/app.js
+++ Week_1/Day3/app.js
@@ -2,10 +2,10 @@ console.log('Commit 1');
 console.log('Commit 2');
 console.log('Commit 3');
 console.log('Commit 4');
-console.log('Commit 5 : Introduce bug'
+console.log('Commit 5 : Introduce bug');
 console.log('Commit 6');
 console.log('Commit 7');
 console.log('Commit 8');
 console.log('Commit 9');
 console.log('Commit 10');
-Stash changes
+"Stash changes"
