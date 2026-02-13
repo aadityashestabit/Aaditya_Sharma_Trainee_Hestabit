afaaf214282e82f3b7e9ac88a2e60d9ad9c8c5e6
Aaditya Sharma <aadityas.hestabit@gmail.com>
2026-02-13 10:52:50 +0530

    add day 4 tasks

---
+++ Week_2/Day4/script.js
@@ -0,0 +1,56 @@
+const inputTask = document.querySelector('#input-box');
+
+const listContainer = document.querySelector('#list-container');
+const addTaskBtn = document.querySelector('.addTaskBtn');
+
+addTaskBtn.addEventListener("click" , function(e){
+    addTask();
+});
+
+
+
+function addTask(){
+    if(inputTask.value == ""){
+        alert("Input field must be filled");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputTask.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        
+        
+    }
+    inputTask.value = "";
+    saveData();
+
+}
+
+
+
+listContainer.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();   
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+
+showTask();
+
