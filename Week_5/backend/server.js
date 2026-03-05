723a287bfd52f0003b5d03fa9c610f1965f85b8d
Aaditya Sharma <aadityas.hestabit@gmail.com>
2026-03-05 15:46:28 +0530

    Add docker day 1 tasks

---
+++ Week_5/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker Container 🚀");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API running inside container" });
+});
+
+app.listen(3000, () => {
+  cgroupsonsole.log("Server running on port 3000");
+});
