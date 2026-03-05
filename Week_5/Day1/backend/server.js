a96dd35e6461713cd9b09b9f154182f8ef57e999
Aaditya Sharma <aadityas.hestabit@gmail.com>
2026-03-05 17:01:56 +0530

    Modify folder structure

--- Week_5/Day1/backend/server.js
+++ Week_5/Day1/backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker Container 🚀");
+  res.send("Hello from Docker Container");
 });
 
 app.get("/api", (req, res) => {
