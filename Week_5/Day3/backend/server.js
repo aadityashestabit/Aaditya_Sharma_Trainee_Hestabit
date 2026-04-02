716acea50df614b3e524bd9ed7140ee07df4d95b
Aaditya Sharma <aadityas.hestabit@gmail.com>
2026-04-02 16:39:49 +0530

    fixed security vulnerabilities

--- Week_5/Day3/backend/server.js
+++ Week_5/Day3/backend/server.js
@@ -4,8 +4,7 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/api", (req, res) => {
-  console.log(req.url)
+app.get("/api", (req, res) => { 
   res.json({
     message: "Hello from backend",
     container: process.env.HOSTNAME
